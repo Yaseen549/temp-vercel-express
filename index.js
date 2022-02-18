271df9b7fc95cbe6bc00c57da05107b528c23206
Yaseen <fantasticyaseenshariff@gmail.com>
2022-02-18 15:17:27 +0530

    update files

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const product = require("./api/product");
 
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
-app.use("/api/product",product);
+app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => console.log(`server is running in port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
